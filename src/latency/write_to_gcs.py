5fb5dce319eb731ac2e532852e655d3b2f181cfd
Gino Filicetti <ginof@google.com>
2023-11-17 15:14:11 -0500

    taking data creation out of the write direct Added csv to the ignore

--- src/latency/write_to_gcs.py
+++ src/latency/write_to_gcs.py
@@ -15,13 +15,13 @@ DIR = "test1"
 def generate_random_data(size_in_bytes):
     return ''.join(chr(random.randint(65, 90)) for _ in range(size_in_bytes))
 
-def generate_file(file_size, file_prefix, file_ext):
+def generate_file(data, file_prefix, file_ext):
     # we only need the 6 least significant digits from the hex of our uuid
     uid = f"{uuid4().hex[-6:]}"
     filename = f"{file_prefix}{uid}.{file_ext}"
 
     with open(filename, "w") as f:
-        f.write(generate_random_data(file_size))
+        f.write(data)
 
     return filename
 
@@ -35,24 +35,15 @@ def write_file_to_gcs(filename):
     blob = bucket.blob(f"{DIR}/sdk-{filename}")
     blob.upload_from_filename(filename)
 
-def write_direct_to_gcs(filename, file_size):
+def write_direct_to_gcs(filename, data):
     storage_client = storage.Client()
     bucket = storage_client.get_bucket(BUCKET)
     blob = bucket.blob(f"{DIR}/direct-{filename}")
 
     with blob.open('w') as f:
-        f.write(generate_random_data(file_size))
+        f.write(data)
 
 def main(args):
-
-    # print("gino")
-    # for count in range(10):
-    #     print(f"{count}", end="\r")
-
-    # for i in tqdm(range(100)):
-    #     time.sleep(1)
-
-    # exit()
 
     file_size = args.file_size
     file_prefix = args.file_prefix
@@ -63,7 +54,8 @@ def main(args):
     writer.writerow(["iteration", "filename", "size_bytes", "time_file_to_gcs", "time_direct_to_gcs"])
 
     for i in range(iterations):
-        filename = generate_file(file_size, file_prefix, file_ext)
+        data = generate_random_data(file_size)
+        filename = generate_file(data, file_prefix, file_ext)
 
         start = time.perf_counter()
         write_file_to_gcs(filename)
@@ -71,11 +63,12 @@ def main(args):
         time_file = int((end - start)*1000%1000)
 
         start = time.perf_counter()
-        write_direct_to_gcs(filename, file_size)
+        write_direct_to_gcs(filename, data)
         end = time.perf_counter()
         time_direct = int((end - start)*1000%1000)
 
         writer.writerow([i, filename, file_size, time_file, time_direct])
+        sys.stdout.flush()
 
 if __name__ == "__main__":
     parser = ArgumentParser(description="Generate random files")
