a40758a1365ffa6e079b311a41654b1b8cb5f2fa
Gino Filicetti <ginof@google.com>
2023-11-20 12:10:45 -0500

    first try at passing arguments into the container

--- scripts/create-cloudrun-job.sh
+++ scripts/create-cloudrun-job.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 # This script will deploy a new cloud run job in the region passed in 
-# create-cloudrun-job.sh {cloudrun_job_name} {region} {image}
+# create-cloudrun-job.sh {cloudrun_job_name} {image} {arguments} {region} 
 JOB=${1:-"gcs-latency-tester"}
-REGION=${2:-"us-central1"}
-IMAGE=${3:-"us-central1-docker.pkg.dev/gcs-latency-poc/registry-docker/gcs-latency-tester"}
+IMAGE=${2:-"us-central1-docker.pkg.dev/gcs-latency-poc/registry-docker/gcs-latency-tester"}
+ARGS=${3:-"10 5000000 file_ ts gcs_latency_test 5MB"}
+REGION=${4:-"us-central1"}
 
 printf "Creating cloud run job: ${JOB} \n"
-printf "Using region: ${REGION} \n"
 printf "Using image: ${IMAGE} \n"
+printf "Using args: ${ARGS} \n"
+printf "Using region: ${REGION} \n"
 
 # create the new cloud run job
-gcloud run jobs create $JOB \
-    --image=$IMAGE:latest \
-    --parallelism=1 \
-    --region=$REGION
+gcloud run jobs create ${JOB} \
+    --image ${IMAGE}:latest \
+    --args ${ARGS} \
+    --parallelism 1 \
+    --region ${REGION}
--- scripts/exec-cloudrun-job.sh
+++ scripts/exec-cloudrun-job.sh
@@ -2,14 +2,14 @@
 # This script will execute the passed in cloud run job
 # exec-cloudrun-job.sh {cloudrun_job_name} {container_args} {env_vars} {--wait}
 JOB=${1:-"gcs-latency-tester"}
-ARGS=${2:-"arg1,arg2,arg3"}
-ENV_VARS=${3:-"VAR1=VAL1,VAR2=VAL2"}
-REGION=${4:-"us-central1"}
+ARGS=${2:-""}
+# ENV_VARS=${3:-"VAR1=VAL1,VAR2=VAL2"}
+REGION=${3:-"us-central1"}
 
 printf "Executing cloud run job: ${JOB} \n"
 
 # execute the cloud run job
 gcloud run jobs execute ${JOB} \
      --args ${ARGS} \
-     --update-env-vars ${ENV_VARS} \
+    #  --update-env-vars ${ENV_VARS} \
      --region ${REGION}
