bffab3dd87e3012bd42e311ddacade718e6b84b2
Gino Filicetti <ginof@google.com>
2023-11-20 13:42:50 -0500

    move args parameter up as it is more common to change than the docker image

--- scripts/create-cloudrun-job.sh
+++ scripts/create-cloudrun-job.sh
@@ -2,8 +2,8 @@
 # This script will deploy a new cloud run job in the region passed in 
 # create-cloudrun-job.sh {cloudrun_job_name} {image} {arguments} {region} 
 JOB=${1:-"gcs-latency-tester"}
-IMAGE=${2:-"us-central1-docker.pkg.dev/gcs-latency-poc/registry-docker/gcs-latency-tester"}
-ARGS=${3:-"10,5000000,file_,ts,gcs_latency_test,5MB"}
+ARGS=${2:-"10,5000000,file_,ts,gcs_latency_test,5MB"}
+IMAGE=${3:-"us-central1-docker.pkg.dev/gcs-latency-poc/registry-docker/gcs-latency-tester"}
 REGION=${4:-"us-central1"}
 
 printf "Creating cloud run job: ${JOB} \n"
