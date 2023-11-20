307847707624a854cecf0414933e00d00b608598
Gino Filicetti <ginof@google.com>
2023-11-20 16:51:32 -0500

    removed the doubling up of default values

--- scripts/exec-cloudrun-job.sh
+++ scripts/exec-cloudrun-job.sh
@@ -2,7 +2,7 @@
 # This script will execute the passed in cloud run job
 # exec-cloudrun-job.sh {cloudrun_job_name} {env_vars} {region}
 JOB=${1:-"gcs-latency-tester"}
-ENV=${2:-'LATENCY_TEST_ITERATIONS=10,LATENCY_TEST_FILE_SIZE=5000000,LATENCY_TEST_FILE_PREFIX="file_",LATENCY_TEST_FILE_EXTENSION="ts",LATENCY_TEST_BUCKET_NAME="gcs-latency-test",LATENCY_TEST_FOLDER_NAME="5MB"'}
+ENV=${2:-""}
 REGION=${3:-"us-central1"}
 
 printf "Executing cloud run job: ${JOB} \n"
@@ -11,5 +11,5 @@ printf "In region: ${REGION} \n"
 
 # execute the cloud run job
 gcloud run jobs execute ${JOB} \
-     --update-env-vars=${ENV} \
-     --region ${REGION}
+     --region ${REGION} \
+     --update-env-vars=${ENV}
