4686635decdc73d566bc35c8e32165c3c326e0a4
Gino Filicetti <ginof@google.com>
2023-11-28 18:44:54 -0500

    Adding Murat's script for reference Adding the pandas requirement to print us all the stats we need

---
+++ src/latency/write_to_gcs_murat.py
@@ -0,0 +1,99 @@
+import argparse
+import io
+import time
+from uuid import uuid4
+
+import pandas as pd
+from google.cloud import storage
+
+
+def read(filename: str) -> bytes:
+    with open(filename, "rb") as f:
+        return f.read()
+
+
+def blob_write(bucket_name: str, data: bytes, n: int) -> list[float]:
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    times = [0] * n
+    for i in range(n):
+        name = uuid4().hex
+        blob = bucket.blob(name)
+        start = time.perf_counter()
+        with blob.open("wb") as f:
+            f.write(data)
+        times[i] = time.perf_counter() - start
+    return times
+
+
+def upload_from_string(bucket_name: str, data: bytes, n: int) -> list[float]:
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    times = [0] * n
+    for i in range(n):
+        name = uuid4().hex
+        blob = bucket.blob(name)
+        start = time.perf_counter()
+        blob.upload_from_string(data)
+        times[i] = time.perf_counter() - start
+    return times
+
+
+def upload_from_filename(bucket_name: str, src_file_name: str, n: int) -> list[float]:
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    times = [0] * n
+    for i in range(n):
+        name = uuid4().hex
+        blob = bucket.blob(name)
+        start = time.perf_counter()
+        blob.upload_from_filename(src_file_name)
+        times[i] = time.perf_counter() - start
+    return times
+
+
+def upload_from_file(bucket_name: str, data: bytes, n: int) -> list[float]:
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    file_obj = io.BytesIO(data)
+    times = [0] * n
+    for i in range(n):
+        name = uuid4().hex
+        blob = bucket.blob(name)
+        start = time.perf_counter()
+        file_obj.seek(0)
+        blob.upload_from_file(file_obj)
+        times[i] = time.perf_counter() - start
+    return times
+
+
+def process(args):
+    data = read(args.src_file_name)
+    t_s = upload_from_string(args.bucket_name, data, args.iterations)
+    t_f = upload_from_file(args.bucket_name, data, args.iterations)
+    t_n = upload_from_filename(args.bucket_name, args.src_file_name, args.iterations)
+    t_w = blob_write(args.bucket_name, data, args.iterations)
+
+    with open(args.output, "w") as f:
+        f.write("from_string, from_file, from_file_name, blob_write\n")
+        rows = zip(t_s, t_f, t_n, t_w)
+        for row in rows:
+            line = ",".join(map(str, row))
+            f.write(f"{line}\n")
+
+
+def summary(output_file_name: str):
+    df = pd.read_csv(output_file_name)
+    print(df.describe())
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--bucket-name", default="meken-playground-blobs")
+    parser.add_argument("--src-file-name", default="random.bytes")
+    parser.add_argument("--output", default="results.csv")
+    parser.add_argument("iterations", type=int, nargs="?", default=1)
+    args = parser.parse_args()
+
+    process(args)
+    summary(args.output)
