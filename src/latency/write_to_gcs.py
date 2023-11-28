c01fdf3ea651072208b8248ab3a9ce85c47eb4a2
Gino Filicetti <ginof@google.com>
2023-11-28 18:45:36 -0500

    Changed my script to use pandas for stats output and to add all 4 methods for xfer'ing files. like Murat's script.

--- src/latency/write_to_gcs.py
+++ src/latency/write_to_gcs.py
@@ -1,4 +1,5 @@
 import os
+import io
 import sys
 import time
 import random
@@ -8,34 +9,58 @@ import datetime
 from uuid import uuid4
 from argparse import ArgumentParser
 
-from google.cloud import storage
+import pandas as pd
+from pydoc import describe
 from tqdm import tqdm
+from google.cloud import storage
 
 def generate_random_data(size_in_bytes):
-    return ''.join(chr(random.randint(65, 90)) for _ in range(size_in_bytes))
+    data_string = ''.join(chr(random.randint(65, 90)) for _ in range(size_in_bytes))
+    bytes = data_string.encode('utf-8')
+    return bytes
 
 def generate_file(data, file_prefix, file_ext):
     # we only need the 6 least significant digits from the hex of our uuid
     uid = f"{uuid4().hex[-6:]}"
-    filename = f"{file_prefix}{uid}.{file_ext}"
+    filename = f"{uid}-{file_prefix}.{file_ext}"
 
-    with open(filename, "w") as f:
+    with open(filename, "wb") as f:
         f.write(data)
 
     return filename
 
-def write_file_to_gcs(bucket, folder, filename):
-    blob = bucket.blob(f"{folder}/sdk-{filename}")
+def write_file_to_gcs(bucket, folder, filename, data):
+    blob = bucket.blob(f"{folder}/by-file-{filename}")
+    file_obj = io.BytesIO(data)
+    file_obj.seek(0)
+    start = time.perf_counter()
+    blob.upload_from_file(file_obj)
+    return time.perf_counter() - start
+
+def write_filename_to_gcs(bucket, folder, filename):
+    blob = bucket.blob(f"{folder}/by-filename-{filename}")
+    start = time.perf_counter()
     blob.upload_from_filename(filename)
+    return time.perf_counter() - start
+
+def write_string_to_gcs(bucket, folder, filename, data):
+    blob = bucket.blob(f"{folder}/by-string-{filename}")
+    start = time.perf_counter()
+    blob.upload_from_string(data)
+    return time.perf_counter() - start
 
 def write_direct_to_gcs(bucket, folder, filename, data):
-    blob = bucket.blob(f"{folder}/direct-{filename}")
+    blob = bucket.blob(f"{folder}/by-stream-{filename}")
 
-    with blob.open('w') as f:
+    start = time.perf_counter()
+
+    with blob.open('wb') as f:
         f.write(data)
 
+    return time.perf_counter() - start
+
 def output_results(test_results, bucket, folder, file_prefix):
-    headers = ["iteration", "filename", "size_mb", "time_file_to_gcs", "time_direct_to_gcs"]
+    headers = ["time_file", "time_filename", "time_string", "time_direct"]
     timestamp = datetime.datetime.now().strftime("%Y%m%d-%H_%M")
 
     # Write results into blob storage
@@ -50,8 +75,11 @@ def output_results(test_results, bucket, folder, file_prefix):
     stdout.writerow(headers)
     stdout.writerows(test_results)
 
-def main(args):
+    # Use panda to get stats 
+    df = pd.DataFrame(test_results, columns=["time_file", "time_filename", "time_string", "time_direct"])
+    print(df.describe())
 
+def main(args):
     iterations = args.iterations
     file_size = args.file_size
     file_prefix = args.file_prefix
@@ -62,23 +90,18 @@ def main(args):
     storage_client = storage.Client()
     bucket = storage_client.get_bucket(bucket_name)
 
-    test_results = []
+    data = generate_random_data(file_size)
+    filename = generate_file(data, file_prefix, file_ext)
+
+    test_results = [0] * iterations
 
     for i in tqdm(range(iterations), desc="Testing GCS Latency", unit="Test", colour="green"):
-        data = generate_random_data(file_size)
-        filename = generate_file(data, file_prefix, file_ext)
+        time_file = write_file_to_gcs(bucket, folder_name, filename, data)
+        time_filename = write_filename_to_gcs(bucket, folder_name, filename)
+        time_string = write_string_to_gcs(bucket, folder_name, filename, data)
+        time_direct = write_direct_to_gcs(bucket, folder_name, filename, data)
 
-        start = time.perf_counter()
-        write_file_to_gcs(bucket, folder_name, filename)
-        end = time.perf_counter()
-        time_file = int((end - start)*1000%1000)
-
-        start = time.perf_counter()
-        write_direct_to_gcs(bucket, folder_name, filename, data)
-        end = time.perf_counter()
-        time_direct = int((end - start)*1000%1000)
-
-        test_results.append([i, filename, int(file_size/10**6), time_file, time_direct])
+        test_results[i] = (time_file, time_filename, time_string, time_direct)
 
     output_results(test_results, bucket, folder_name, file_prefix)
 
@@ -87,10 +110,9 @@ if __name__ == "__main__":
     parser = ArgumentParser(description="Generate random files")
     parser.add_argument("--iterations", type=int, help="Number of times to run the test", default=os.getenv("LATENCY_TEST_ITERATIONS", 10))
     parser.add_argument("--file-size", type=int, help="Size of each file (in bytes)", default=os.getenv("LATENCY_TEST_FILE_SIZE", 5*(10**6)))
-    parser.add_argument("--file-prefix", type=str, help="Filename prefix to use", default=os.getenv("LATENCY_TEST_FILE_PREFIX", "file_"))
+    parser.add_argument("--file-prefix", type=str, help="Filename prefix to use", default=os.getenv("LATENCY_TEST_FILE_PREFIX", "file"))
     parser.add_argument("--file-ext", type=str, help="File extension to use", default=os.getenv("LATENCY_TEST_FILE_EXTENSION", "ts"))
     parser.add_argument("--bucket-name", type=str, help="Name of GCS Bucket", default=os.getenv("LATENCY_TEST_BUCKET_NAME", "gcs-latency-test"))
     parser.add_argument("--folder-name", type=str, help="Folder in bucket to use", default=os.getenv("LATENCY_TEST_FOLDER_NAME", "5MB"))
-
 
     main(parser.parse_args())
