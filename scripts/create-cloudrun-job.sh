29af142fb2580736958a48425f61490d67dd9fe8
Gino Filicetti <ginof@google.com>
2023-11-29 10:25:28 -0500

    slight update to the default file prefix

--- scripts/create-cloudrun-job.sh
+++ scripts/create-cloudrun-job.sh
@@ -2,7 +2,7 @@
 # This script will deploy a new cloud run job in the region passed in 
 # create-cloudrun-job.sh {cloudrun_job_name} {image} {env_vars} {region} 
 JOB=${1:-"gcs-latency-tester"}
-ENV=${2:-"LATENCY_TEST_ITERATIONS=10,LATENCY_TEST_FILE_SIZE=5000000,LATENCY_TEST_FILE_PREFIX=file_,LATENCY_TEST_FILE_EXTENSION=ts,LATENCY_TEST_BUCKET_NAME=gcs-latency-test,LATENCY_TEST_FOLDER_NAME=5MB"}
+ENV=${2:-"LATENCY_TEST_ITERATIONS=10,LATENCY_TEST_FILE_SIZE=5000000,LATENCY_TEST_FILE_PREFIX=file,LATENCY_TEST_FILE_EXTENSION=ts,LATENCY_TEST_BUCKET_NAME=gcs-latency-test,LATENCY_TEST_FOLDER_NAME=5MB"}
 IMAGE=${3:-"us-central1-docker.pkg.dev/gcs-latency-poc/registry-docker/gcs-latency-tester"}
 REGION=${4:-"us-central1"}
 
