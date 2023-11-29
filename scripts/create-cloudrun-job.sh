f785c02689221bb75a97c3ef2e774714eb5d86d2
Gino Filicetti <ginof@google.com>
2023-11-29 13:59:04 -0500

    Fixed the ordering of parameters in the cmd line documentation

--- scripts/create-cloudrun-job.sh
+++ scripts/create-cloudrun-job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This script will deploy a new cloud run job in the region passed in 
-# create-cloudrun-job.sh {cloudrun_job_name} {image} {env_vars} {region} 
+# create-cloudrun-job.sh {cloudrun_job_name} {env_vars} {image} {region} 
 JOB=${1:-"gcs-latency-tester"}
 ENV=${2:-"LATENCY_TEST_ITERATIONS=10,LATENCY_TEST_FILE_SIZE=5000000,LATENCY_TEST_FILE_PREFIX=file,LATENCY_TEST_FILE_EXTENSION=ts,LATENCY_TEST_BUCKET_NAME=gcs-latency-test,LATENCY_TEST_FOLDER_NAME=5MB"}
 IMAGE=${3:-"us-central1-docker.pkg.dev/gcs-latency-poc/registry-docker/gcs-latency-tester"}
